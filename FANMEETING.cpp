69ac92182676a9f9ced03028628f9c5abc4a0207
김성민 <tolelom@mail.hongik.ac.kr>
2021-09-05 20:48:28 +0900

    Update FANMEETING.cpp

--- FANMEETING.cpp
+++ FANMEETING.cpp
@@ -1,14 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void normalize(vector<int>& num) {
+    num.push_back(0);
+
+    for (int i = 0; i < num.size(); ++i) {
+        if (num[i] < 0) {
+            int borrow = (abs(num[i]) + 9) / 10;
+            num[i + 1] -= borrow;
+            num[i] += borrow * 10;
+        } else {
+            num[i + 1] += num[i] / 10;
+            num[i] %= 10;
+        }
+    }
+
+    while(num.size() > 1 && num.back() == 0) num.pop_back();
+}
+
+vector<int> multiply(const vector<int>& a, const vector<int>& b) {
+    vector<int> c(a.size() + b.size() + 1, 0);
+    for (int i = 0; i < a.size(); ++i) 
+        for (int j = 0; j < b.size(); ++j)
+            c[i + j] += a[i] * b[j];
+
+    normalize(c);
+    return c;
+}
+
 
 void addTo(vector<int>& a, const vector<int>& b, int k) {
-
+    int newsize = a.size() < b.size() + k ? b.size() + k : a.size();
+    while (a.size() != newsize) a.push_back(0);
+    for (int i = k; i < newsize; i++) {
+        a[i] = a[i] + b[i - k];
+    }
+    normalize(a);
 }
-
-void subFrom(vector<int>& a, const vector<int>& b) {
-
+ 
+void subFrom(vector<int> &a, const vector<int>& b) {
+    for (int i = 0; i < b.size(); i++) {
+        a[i] -= b[i];
+    }
+    normalize(a);
 }
+ 
 
 vector<int> karatsuba(const vector<int>& a, const vector<int>& b) {
     int an = a.size(), bn = b.size();
